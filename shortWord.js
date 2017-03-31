68ecb21211bff690f98df9b19476ef0dedca3a40
sbrown17 <stubrown357@gmail.com>
2017-03-31 00:07:55 -0400

    Add files via upload

---
+++ shortWord.js
@@ -0,0 +1,20 @@
+//function to determine shortest word
+function findShort (s) {
+
+//creating variables for words string and the shortest word
+    var str = s.split(' ');
+    var firstWord = str[0].length;
+
+    //compare words to each other
+    for (var i = 1; i < str.length ; i++) {
+
+        if(firstWord >= str[i].length){
+            firstWord = str[i].length;
+        } else {
+        	return firstWord;
+        }
+
+    }
+    
+}
+
